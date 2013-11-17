42c08e2a42271ec1a4c8f39123ad75094c002717
José María Campaña <chemisax@gmail.com>
2013-11-18 00:53:18 +0900

    生きてる！
    Change in CSS
    Made images smaller
    Other changes that I dont remember
    Made fish appear random
    Made spaces for types of fish

---
+++ fish_catalog.js
@@ -0,0 +1,41 @@
+var fish_catalog = new Array(3);
+
+fish_catalog[0] = new Array(
+	{
+		name: "    Beluga",
+		file: "./img/fish_anime/fish1.png",
+		height: 70,
+		width: 110.5 
+	},
+	{
+		name: "Normal 2",
+		file: "./img/fish_anime/fish2.png",
+		height: 70,
+		width: 87
+	}
+);
+
+fish_catalog[1] = new Array(
+	{
+		name: "Rare 1",
+		file: "./img/fish_anime/fish3.png",
+		height: 70,
+		width: 77.5 
+	},
+	{
+		name: "Rare 2",
+		file: "./img/fish_anime/fish4.png",
+		height: 70,
+		width: 137.5 
+	}
+	
+);
+
+fish_catalog[2] = new Array(
+	{
+		name: "Shita 1",
+		file: "./img/fish_anime/fish5.png",
+		height: 70,
+		width: 70 
+	}
+);
